881c8b22147c5d08744ff5963f4628adfaf15e49
iVanz-git <107778327+iVanz-git@users.noreply.github.com>
2022-08-22 17:48:34 +0800

    Update arrayandstringtest.java
    在github上更改测试

--- arrayandstringtest.java
+++ arrayandstringtest.java
@@ -2,13 +2,11 @@ public class arrayandstringtest {
     public static void main(String[] args) {
 
         //1.替换字符串中特定字符为要求内容
-        //替换所有"a"为"*"
+        //替换所有"a"为"_"
 
         String s1 = new String("sadasdaasds1 d1 324 1aas15fdhdf");
         String s2 = arrayandstringtest.f2(s1);
-        System.out.println(s2);
-
-        
+        System.out.println(s2);        
     
     }
 
@@ -18,7 +16,7 @@ public class arrayandstringtest {
             StringBuilder stringBuilder = new StringBuilder();
             for (char c : str.toCharArray()) {
                 if (c == 'a') {
-                    stringBuilder.append("*");
+                    stringBuilder.append("_");
                     
                 }
                 else{
